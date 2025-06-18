7d16b55cfc33a500293be1aa02688dd420d022b3
Fausto C <faustocamp545@gmail.com>
2025-06-17 19:22:42 -0500

    feat: add browser router into app component

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router'
+import Home from './pages/Home'
+import About from './pages/About'
+import Vans from './pages/Vans'
+import VanDetail from './pages/VanDetail'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className='site-logo' to='/'>
+          #VanLife
+        </Link>
+        <nav>
+          <Link to='/about'>About</Link>
+          <Link to='/vans'>Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
+        <Route path='/vans/:id' element={<VanDetail />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
