7adb6d78e2baad1a7972950401295ff722b10050
Fausto C <faustocamp545@gmail.com>
2025-06-24 15:50:20 -0500

    feat: replace link by navlink react-router component

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,13 +1,28 @@
-import { Link } from 'react-router'
+import { NavLink } from 'react-router'
 import { Outlet } from 'react-router'
 
 const HostLayout = () => {
+  function isActiveStyle({ isActive }) {
+    if (isActive) {
+      return {
+        fontWeight: 'bold',
+        textDecoration: 'underline',
+        color: '#161616',
+      }
+    }
+  }
   return (
     <>
-      <nav>
-        <Link to='/host'>Dashboard</Link>
-        <Link to='/host/income'>Income</Link>
-        <Link to='/host/reviews'>Reviews</Link>
+      <nav className='host-nav'>
+        <NavLink to='/host' style={isActiveStyle} end>
+          Dashboard
+        </NavLink>
+        <NavLink to='/host/income' style={isActiveStyle}>
+          Income
+        </NavLink>
+        <NavLink to='/host/reviews' style={isActiveStyle}>
+          Reviews
+        </NavLink>
       </nav>
       <Outlet />
     </>
