35213e90a8ae6dec944377ff4882e0253e2902fb
Fausto C <faustocamp545@gmail.com>
2025-08-19 21:39:46 -0500

    chore: Define Layout with bootstrap classes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router'
 import Header from './Header'
 import Footer from './Footer'
+
 const Layout = () => {
   return (
-    <div className='site-wrapper'>
+    <div className='d-flex flex-column min-vh-100'>
       <Header />
-      <main>
+      <main className='flex-grow-1'>
         <Outlet />
       </main>
       <Footer />
