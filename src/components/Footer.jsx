c8135a091ded2cea529d1989ed9a78a93911a14f
Fausto C <faustocamp545@gmail.com>
2025-08-19 21:40:42 -0500

    chore: Update footer's style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,12 @@
 const Footer = () => {
-  return <footer>&#169; 2025 #VANLIFE</footer>
+  return (
+    <footer
+      className='text-center fw-medium text-bg-dark text-secondary d-flex align-items-center justify-content-center flex-shrink-0'
+      style={{ height: '74px' }}
+    >
+      &#169; 2025 #VANLIFE
+    </footer>
+  )
 }
 
 export default Footer
