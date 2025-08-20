bdfea56958085a1a56237b1a106564db09a07374
Fausto C <faustocamp545@gmail.com>
2025-08-20 18:08:50 -0500

    chore: Update style classes of Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import { Link } from 'react-router'
 const Home = () => {
   return (
-    <div className='home-container'>
-      <h1>You got the travel plans, we got the travel vans</h1>
-      <p>
+    <div className='home-container d-flex flex-column justify-content-center p-4 p-md-5'>
+      <h1 className='fw-bold fs-2 lh-base'>
+        You got the travel plans, we got the travel vans
+      </h1>
+      <p className='lh-sm'>
         Add adventure to your life by joining the #vanlife movement. Rent the
         perfect van to make your perfect road trip
       </p>
-      <Link to='vans'>Find your van</Link>
+      <Link to='vans' className='link-button bg-primary w-100 mt-4'>
+        Find your van
+      </Link>
     </div>
   )
 }
