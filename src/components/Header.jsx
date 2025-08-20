c2ea5e2429e50676bf379f23096edc24245c6fc1
Fausto C <faustocamp545@gmail.com>
2025-08-19 21:41:20 -0500

    chore: Update header's style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,33 @@
 import { Link, NavLink } from 'react-router'
 const Header = () => {
   return (
-    <header>
+    <header className='sticky-top' style={{ height: '110px' }}>
+      <nav className='navbar h-100'>
+        <div className='container-fluid'>
+          <Link className='navbar-brand fw-bold text-uppercase' to='/'>
+            #VanLife
+          </Link>
+          <div className='d-flex gap-2 fw-semibold'>
+            <NavLink
+              to='/host'
+              className={({ isActive }) =>
+                `link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-75-hover ${isActive ? 'link-dark fw-bold' : ''}`
+              }
+            >
+              Host
+            </NavLink>
+            <NavLink to='/about'>About</NavLink>
+            <NavLink to='/vans'>Vans</NavLink>
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
+{
+  /* <header>
       <Link className='site-logo' to='/'>
         #VanLife
       </Link>
@@ -24,9 +50,6 @@ const Header = () => {
         >
           Vans
         </NavLink>
-      </nav>
-    </header>
-  )
+        </nav> 
+      </header> */
 }
-
-export default Header
