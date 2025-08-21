530605a68346c5b4875f512001049e49a61eb454
Fausto C <faustocamp545@gmail.com>
2025-08-21 16:49:27 -0500

    chore: Update styles of About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,31 +2,38 @@ import { Link } from 'react-router'
 import bgImage from '../assets/images/about-hero.png'
 const About = () => {
   return (
-    <div className='about-page-container'>
-      <img src={bgImage} className='about-hero-image' />
-      <div className='about-page-content'>
-        <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
-        <p>
-          Our mission is to enliven your road trip with the perfect travel van
-          rental. Our vans are recertified before each trip to ensure your
-          travel plans can go off without a hitch. (Hitch costs extra 😉)
-        </p>
-        <p>
-          Our team is full of vanlife enthusiasts who know firsthand the magic
-          of touring the world on 4 wheels.
-        </p>
+    <>
+      <img src={bgImage} className='img-fluid' />
+      <div className='px-4 py-5'>
+        <div className='mb-5'>
+          <h1 className='fw-bold lh-lg'>
+            Don’t squeeze in a sedan when you could relax in a van.
+          </h1>
+          <p className='lh-sm'>
+            Our mission is to enliven your road trip with the perfect travel van
+            rental. Our vans are recertified before each trip to ensure your
+            travel plans can go off without a hitch. (Hitch costs extra 😉)
+          </p>
+          <p className='lh-sm'>
+            Our team is full of vanlife enthusiasts who know firsthand the magic
+            of touring the world on 4 wheels.
+          </p>
+        </div>
+        <div
+          className='px-4 pb-4 rounded-1'
+          style={{ backgroundColor: '#ffcc8d' }}
+        >
+          <h2 className='m-0 py-5 fw-semibold'>
+            Your destination is waiting.
+            <br />
+            Your van is ready.
+          </h2>
+          <Link className='link-button text-bg-dark' to='/vans'>
+            Explore our vans
+          </Link>
+        </div>
       </div>
-      <div className='about-page-cta'>
-        <h2>
-          Your destination is waiting.
-          <br />
-          Your van is ready.
-        </h2>
-        <Link className='link-button' to='/vans'>
-          Explore our vans
-        </Link>
-      </div>
-    </div>
+    </>
   )
 }
 
