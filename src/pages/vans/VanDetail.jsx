d5d311901efae6fc9dfd97f1179e558b6a15b0f3
Fausto C <faustocamp545@gmail.com>
2025-08-27 20:49:13 -0500

    chore: Update Van Detail style

--- src/pages/vans/VanDetail.jsx
+++ src/pages/vans/VanDetail.jsx
@@ -12,17 +12,25 @@ const VanDetail = () => {
   }, [params.id])
 
   return (
-    <div className='van-detail-container'>
+    <div className='p-4 pb-5'>
       {van ? (
-        <div className='van-detail'>
-          <img src={van.imageUrl} alt={van.name} />
-          <i className={`van-type ${van.type} selected`}>{van.type}</i>
-          <h2>{van.name}</h2>
-          <p className='van-price'>
-            <span>${van.price}</span>/day
+        <div className='d-flex flex-column'>
+          <img
+            src={van.imageUrl}
+            alt={van.name}
+            className='img-fluid rounded mb-5'
+          />
+          <i className={`badge van-type ${van.type} selected align-self-start`}>
+            {van.type}
+          </i>
+          <h2 className='fs-2 my-2'>{van.name}</h2>
+          <p className='mb-2'>
+            <span className='fw-bold fs-5'>${van.price}</span>/day
           </p>
           <p>{van.description}</p>
-          <button className='link-button'>Rent this van</button>
+          <button className='link-button bg-primary text-white align-self-md-start'>
+            Rent this van
+          </button>
         </div>
       ) : (
         <h2>Loading...</h2>
