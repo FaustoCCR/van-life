6c94d640caf5785208ec4201db56fe3e4149a540
Fausto C <faustocamp545@gmail.com>
2025-09-01 21:10:34 -0500

    feat: Use Navbar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,46 +1,17 @@
 import { Link, NavLink } from 'react-router'
+import Navbar from './shared/Navbar'
 const Header = () => {
   return (
     <header className='sticky-top' style={{ height: '110px' }}>
-      <nav className='navbar h-100'>
-        <div className='container-fluid'>
-          <Link className='navbar-brand fw-bold text-uppercase' to='/'>
-            #VanLife
-          </Link>
-          <ul className='navbar-nav flex-row nav-underline fw-semibold'>
-            <li className='nav-item'>
-              <NavLink
-                to='/host'
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-              >
-                Host
-              </NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink
-                to='/about'
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-              >
-                About
-              </NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink
-                to='/vans'
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-              >
-                Vans
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Navbar
+        className='h-100'
+        brand={{ to: '/', children: '#VanLife' }}
+        items={[
+          { to: '/host', children: 'Host' },
+          { to: '/about', children: 'About' },
+          { to: '/vans', children: 'Vans' },
+        ]}
+      />
     </header>
   )
 }
--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,29 +1,16 @@
-import { NavLink } from 'react-router'
 import { Outlet } from 'react-router'
+import Navbar from './shared/Navbar'
 
 const HostLayout = () => {
-  function isActiveStyle({ isActive }) {
-    if (isActive) {
-      return {
-        fontWeight: 'bold',
-        textDecoration: 'underline',
-        color: '#161616',
-      }
-    }
-  }
   return (
     <>
-      <nav className='host-nav'>
-        <NavLink to='/host' style={isActiveStyle} end>
-          Dashboard
-        </NavLink>
-        <NavLink to='/host/income' style={isActiveStyle}>
-          Income
-        </NavLink>
-        <NavLink to='/host/reviews' style={isActiveStyle}>
-          Reviews
-        </NavLink>
-      </nav>
+      <Navbar
+        items={[
+          { to: '/host', children: 'Dashboard', end: true },
+          { to: '/host/income', children: 'Income' },
+          { to: '/host/reviews', children: 'Reviews' },
+        ]}
+      />
       <Outlet />
     </>
   )
