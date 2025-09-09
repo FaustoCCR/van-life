e49985660d5e4b2117e030e643892385fdf53db0
Fausto C <faustocamp545@gmail.com>
2025-09-09 15:05:52 -0500

    feat: Add useFetch custom Hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,48 @@
+import { useState, useEffect, useCallback } from 'react'
+/**
+ * Custom hook to fetch data from an API endpoint.
+ * @param {string} url
+ * @param {RequestInit} options
+ * @returns { {data: any, loading: boolean, error: Error | null} }
+ */
+export default function useFetch(url, options = {}) {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  const fetchData = useCallback(
+    async abortController => {
+      setLoading(true)
+      try {
+        const response = await fetch(url, {
+          ...options,
+          signal: abortController.signal,
+        })
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        const result = await response.json()
+        setData(result)
+      } catch (error) {
+        if (error.name === 'AbortError') {
+          return
+        }
+        setError(error)
+      } finally {
+        setLoading(false)
+      }
+    },
+    [url],
+  )
+
+  useEffect(() => {
+    const abortController = new AbortController()
+    fetchData(abortController)
+
+    return () => {
+      abortController.abort()
+    }
+  }, [fetchData])
+
+  return { data, loading, error, refetch: fetchData }
+}
